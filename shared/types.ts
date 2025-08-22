60df4eed7d833653f24b81b263df4b78d5204ab6
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-23 00:06:05 +0530

    feat: add login schema validation using Zod

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 
 
 export type SuccessResponse<T = void> = {
@@ -16,3 +17,8 @@ export type ErrorResponse = {
     error: string;
     isFormError?: boolean;
 }
+
+export const loginSchema = z.object({
+    username: z.string().min(3).max(31).regex(/^[a-zA-Z0-9_]+$/),
+    password: z.string().min(3).max(255)
+})
