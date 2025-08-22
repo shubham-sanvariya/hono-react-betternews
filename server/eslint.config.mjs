f60e6dc45a0350d829b97a5bd65ba524d12900d6
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-22 20:22:59 +0530

    chore: update ESLint and Prettier configurations; add drizzle plugin and compatibility support

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,6 +2,9 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import drizzlePlugin from "eslint-plugin-drizzle"
+import eslintPrettierConfig from "eslint-config-prettier";
+import { fixupPluginRules } from "@eslint/compat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
