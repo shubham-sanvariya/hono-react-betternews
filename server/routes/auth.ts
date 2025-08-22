87fdef492d92267a284ad481e675852bb77b215a
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-23 00:40:35 +0530

    feat: refactor auth routes for improved readability and structure

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,40 +1,89 @@
-import type { Context } from "@/context";
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { loginSchema, type SuccessResponse } from "@/shared/types.ts";
-import { generateId } from "lucia";
+import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
+
 import { db } from "@/adapter";
+import type { Context } from "@/context";
 import { userTable } from "@/db/schema/auth";
 import { lucia } from "@/lucia";
-import postgres from 'postgres';
-import { HTTPException } from "hono/http-exception";
+import { zValidator } from "@hono/zod-validator";
+import { generateId } from "lucia";
+import postgres from "postgres";
 
+import { loginSchema, type SuccessResponse } from "@/shared/types.ts";
 
-export const authRouter = new Hono<Context>().post("/signup", zValidator("form", loginSchema) ,async (c) => {
-    const  { username, password } = c.req.valid("form");
+export const authRouter = new Hono<Context>()
+  .post("/signup", zValidator("form", loginSchema), async (c) => {
+    const { username, password } = c.req.valid("form");
     const passwordHash = await Bun.password.hash(password);
     const userId = generateId(15);
 
     try {
-        await db.insert(userTable).values({
-            id: userId,
-            username,
-            password_hash: passwordHash
-        })
+      await db.insert(userTable).values({
+        id: userId,
+        username,
+        password_hash: passwordHash,
+      });
 
-        const session = await lucia.createSession(userId, { username })
-        const sessionCookie = lucia.createSessionCookie(session.id).serialize();
+      const session = await lucia.createSession(userId, { username });
+      const sessionCookie = lucia.createSessionCookie(session.id).serialize();
 
-        c.header("Set-Cookie", sessionCookie, { append : true })
+      c.header("Set-Cookie", sessionCookie, { append: true });
 
-      return c.json<SuccessResponse>({
-        success: true,
-        message: "User Created"
-      },201)
+      return c.json<SuccessResponse>(
+        {
+          success: true,
+          message: "User Created",
+        },
+        201,
+      );
     } catch (error) {
-      if (error instanceof postgres.PostgresError && error.code === "23505"){
-        throw new HTTPException(409, { message : "Username already exists"} );
+      if (error instanceof postgres.PostgresError && error.code === "23505") {
+        throw new HTTPException(409, { message: "Username already exists" });
       }
-      throw new HTTPException(500, { message : "Failed to create user"});
+      throw new HTTPException(500, { message: "Failed to create user" });
     }
-})
+  })
+  .post("/login", zValidator("form", loginSchema), async (c) => {
+    const { username, password } = c.req.valid("form");
+
+    const [existingUser] = await db
+      .select()
+      .from(userTable)
+      .where(eq(userTable.username, username))
+      .limit(1);
+
+    if (!existingUser){
+      throw new HTTPException(401, {
+        message: "Incorrect username"
+      })
+    }
+
+    const validPassword = await Bun.password.verify(password, existingUser.password_hash);
+
+    if (!validPassword){
+      throw new HTTPException(401, { message: "Incorrect password"});
+    }
+
+    return c.json<SuccessResponse>(
+      {
+        success: true,
+        message: "Logged in",
+      },
+      200,
+    );
+  }).get("/logout", async (c) => {
+    const session = c.get("session")
+    if (!session){
+      return c.redirect("/");
+    }
+
+    await lucia.invalidateSession(session.id);
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize());
+
+    return c.redirect("/")
+  });
+
+
+
+
