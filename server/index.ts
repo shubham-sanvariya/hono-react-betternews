f9e3beec4624484aa9541c15ea47e35efa6ea38d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-22 23:55:32 +0530

    feat: implement session management middleware with CORS support

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,30 @@
 import { type ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
+import { lucia } from "./lucia";
+import type { Context } from "./context";
 
-const app = new Hono();
+const app = new Hono<Context>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.use("*", cors() ,async (c, next) => {
+  const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
+  if (!sessionId) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId);
+  if (session && session.fresh) {
+    c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
+  }
+  if (!session) {
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
+  }
+  c.set("session", session);
+  c.set("user", user);
+  return next();
 });
 
 app.onError((err, c) => {
