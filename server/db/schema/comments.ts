ab492357d63313e6eddc412a8e08158903c5d882
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-23 19:01:47 +0530

    feat: add comments table schema for managing comments in the database

---
+++ server/db/schema/comments.ts
@@ -0,0 +1,16 @@
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+
+export const commentsTable = pgTable("comments", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull(),
+  postId: integer("post_id").notNull(),
+  parentCommentId: integer("parent_comment_id"),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at",{
+    withTimezone: true
+  }).defaultNow().notNull(),
+  depth: integer("depth").default(0).notNull(),
+  commentCount: integer("comment_count").default(0).notNull(),
+  points: integer("points").default(0).notNull()
+})
