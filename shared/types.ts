42c2a7b19106e5a0d6b2f85c3f3b9c81d73e6cad
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-23 21:26:56 +0530

    feat: add createPostSchema for post creation validation

--- shared/types.ts
+++ shared/types.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { insertPostSchema } from "@/db/schema/post.ts";
 
 
 export type SuccessResponse<T = void> = {
@@ -22,3 +23,14 @@ export const loginSchema = z.object({
     username: z.string().min(3).max(31).regex(/^[a-zA-Z0-9_]+$/),
     password: z.string().min(3).max(255)
 })
+
+export const createPostSchema = insertPostSchema
+  .pick({
+  title: true,
+  url: true,
+  content: true
+})
+  .refine((data) => data.url || data.content, {
+    message: "Either URL or Content must be provided",
+    path: ["url", "content"],
+  });
