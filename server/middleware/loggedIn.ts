7554e62ee9312b6cd74a2ef84bff7631c7e962fd
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-23 18:50:45 +0530

    feat: add loggedIn middleware for user authentication

---
+++ server/middleware/loggedIn.ts
@@ -0,0 +1,14 @@
+
+import { HTTPException } from "hono/http-exception";
+
+import type { Context } from "@/context.ts";
+import {createMiddleware} from "hono/factory";
+
+export const loggedIn = createMiddleware<Context>(async (c, next) => {
+  const user = c.get("user");
+  if (!user) {
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
+
+  await next();
+});
