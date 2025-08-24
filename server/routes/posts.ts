f730f577ad2d3ec289a8a9ffb914b6443320e199
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-24 14:08:27 +0530

    feat: implement post upvote functionality with transaction handling

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -8,6 +8,7 @@ import { postTable } from "@/db/schema/post.ts";
 import { postUpvotesTable } from "@/db/schema/upvotes.ts";
 import { loggedIn } from "@/middleware/loggedIn.ts";
 import { zValidator } from "@hono/zod-validator";
+import z from "zod";
 
 import {
   createPostSchema,
@@ -17,6 +18,7 @@ import {
   type SuccessResponse,
 } from "@/shared/types.ts";
 import { getISOFormatDateQuery } from "@/lib/utils.ts";
+import { HTTPException } from "hono/http-exception";
 
 export const postRouter = new Hono<Context>()
   .post("/", loggedIn, zValidator("form", createPostSchema), async (c) => {
@@ -110,13 +112,73 @@ export const postRouter = new Hono<Context>()
 
     const posts = await postQuery;
 
-    return c.json<PaginatedResponse<Post[]>>({
-      data: posts as Post[],
-      success: true,
-      message: "Posts fetched",
-      pagination: {
-        page: page,
-        totalPages: Math.ceil(count.count / limit) as number
+    return c.json<PaginatedResponse<Post[]>>(
+      {
+        data: posts as Post[],
+        success: true,
+        message: "Posts fetched",
+        pagination: {
+          page: page,
+          totalPages: Math.ceil(count.count / limit) as number,
+        },
       },
-    }, 200);
-  });
+      200,
+    );
+  })
+  .post(
+    "/:id/update",
+    loggedIn,
+    zValidator("param", z.object({ id: z.coerce.number() })),
+    async (c) => {
+      const { id } = c.req.valid("param");
+      const user = c.get("user")!;
+      let pointsChange: -1 | 1 = 1;
+
+      const points = await db.transaction(async (tx) => {
+        const [existingUpvote] = await db
+          .select()
+          .from(postUpvotesTable)
+          .where(
+            and(
+              eq(postUpvotesTable.postId, id),
+              eq(postUpvotesTable.userId, user.id),
+            ),
+          )
+          .limit(1);
+
+        pointsChange = existingUpvote ? -1 : 1;
+
+        const [updated] = await tx
+          .update(postTable)
+          .set(
+            { points: sql`${postTable.points} + ${pointsChange}` }
+          )
+          .where(eq(postTable.id, id))
+          .returning(
+            { points: postTable.points}
+          )
+
+        if (!updated){
+          throw new HTTPException(404, { message: "Post not found"});
+        }
+
+        if (existingUpvote){
+          await tx
+            .delete(postUpvotesTable)
+            .where(eq(postUpvotesTable.id, existingUpvote.id));
+        }else {
+          await tx
+            .insert(postUpvotesTable)
+            .values({ postId: id, userId: user.id})
+        }
+
+        return updated.points;
+      });
+
+      return c.json<SuccessResponse<{ count: number; isUpvoted : boolean }>>({
+        success: true,
+        message: "Post updated",
+        data: { count: points, isUpvoted: pointsChange > 0 }
+      });
+    },
+  );
