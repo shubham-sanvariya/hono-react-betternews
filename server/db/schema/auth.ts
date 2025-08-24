a5175910292901197b12e4bb2935024599445687
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-08-24 15:37:29 +0530

    feat: add user relations for posts, comments, and upvotes in database schema

--- server/db/schema/auth.ts
+++ server/db/schema/auth.ts
@@ -1,5 +1,9 @@
 
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { commentsTable } from "./comments";
+import { postTable } from "./post";
+import { commentUpvotesTable, postUpvotesTable } from "./upvotes";
 
 
 export const userTable = pgTable("user", {
@@ -7,6 +11,15 @@ export const userTable = pgTable("user", {
     username: text("username").notNull().unique(),
     password_hash: text("password_hash").notNull(),
 });
+
+export const userRelations = relations(userTable, ({ many }) => ({
+    posts:  many(postTable, { relationName: "author"}),
+    comments: many(commentsTable, { relationName: "author"}),
+    postUpvotes: many(postUpvotesTable, { relationName: "postUpvotes"}),
+    commentUpVotes: many(commentUpvotesTable, {
+        relationName: "commentUpvotes"
+    })
+}));
 
 export const sessionTable = pgTable("session", {
     id: text("id").primaryKey(),
