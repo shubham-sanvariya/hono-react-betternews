392c660daeb884fc77c8c5b2c00c29f398b44b26
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-09-19 17:32:59 +0530

    adding postcard component in the root page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,15 +5,17 @@ import { orderSchema, sortBySchema } from '@/shared/types'
 import { infiniteQueryOptions, useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { getPosts } from '@/lib/api';
 import { SortBar } from "@/components/sort-bar";
+import { PostCard } from '@/components/post-card';
+import { Button } from '@/components/ui/button';
 
 const homeSearchSchema = z.object({
   sortBy: fallback(sortBySchema, "points").default("recent"),
-  order : fallback(orderSchema, "desc").default("desc"),
+  order: fallback(orderSchema, "desc").default("desc"),
   author: z.optional(fallback(z.string(), "")),
   site: z.optional(fallback(z.string(), ""))
 });
 
-const postsInfiniteQueryOptions = ({ sortBy, order, author, site } : z.infer<typeof homeSearchSchema>) => infiniteQueryOptions({
+const postsInfiniteQueryOptions = ({ sortBy, order, author, site }: z.infer<typeof homeSearchSchema>) => infiniteQueryOptions({
   queryKey: ["posts", sortBy, order, author, site],
   queryFn: ({ pageParam }) => getPosts({
     pageParam,
@@ -43,19 +45,26 @@ export const Route = createFileRoute('/')({
 function HomeComponent() {
 
   const { sortBy, order, author, site } = Route.useSearch();
-  const { data, isFetchingNextPage, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery(postsInfiniteQueryOptions( { sortBy, order, author, site} ));
+  const { data, isFetchingNextPage, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery(postsInfiniteQueryOptions({ sortBy, order, author, site }));
 
   return (
     <div className="mx-auto max-w-3xl p-4">
       <h1 className='mb-6 text-2xl font-bold text-foreground'>Submissions</h1>
-      <SortBar sortBy={sortBy} order={order}/>
-      {data?.pages.map((page) => (
-        page.data.map((post) => (
-          <div>
-            {post.title}
-          </div>
-        ))
-      ))}
+      <SortBar sortBy={sortBy} order={order} />
+      <div className='space-y-4'>
+        {data?.pages.map((page) => (
+          page.data.map((post) => (
+            <PostCard post={post} key={post.id} />
+          ))
+        ))}
+        <div className='mt-6'>
+          <Button onClick={() => fetchNextPage()} 
+              disabled={!hasNextPage || isFetchingNextPage}
+            >
+              {isFetchingNextPage ? "Loading more..." : hasNextPage ?  "Load more" : "Nothing more"}
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
